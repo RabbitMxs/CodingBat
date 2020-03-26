a2156a725dbcca4bf9e3d000d89675047a2710aa
RabbitMxs <jorge_epg@outlook.com>
2020-03-25 20:56:28 -0600

    ardilla

---
+++ ardillas.php
@@ -0,0 +1,42 @@
+<?php 
+include "formularioardillas.php";
+$result="";
+if($_POST['verano'] == "1"){
+    $isSummer=true;
+}else{
+    $isSummer=false;
+}
+$temp=$_POST['temp']; 
+    if($isSummer){
+
+        if ($temp>=60 && $temp<=100){
+            $juegan="true";
+            //echo $juegan;
+            $result.='<div class="result1"><label>'.$juegan.'</label> </div>';
+            echo $result;
+
+        }else{
+            $juegan="false";
+            //echo $juegan;
+            $result.='<div class="result1"><label>'.$juegan.'</label> </div>';
+            echo $result;
+
+        }
+    }
+    else{
+
+        if ($temp>=60 && $temp<=90){
+            $juegan="true";
+            //echo $juegan;
+            $result.='<div class="result1"><label>'.$juegan.'</label> </div>';
+            echo $result;
+        }else{
+            $juegan="false";
+            //echo $juegan;
+            $result.='<div class="result1"><label>'.$juegan.'</label> </div>';
+            echo $result;
+
+        }  
+}
+return  $result;
+?>
